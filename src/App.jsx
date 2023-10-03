bb7bdec146333c30e097112d883bc03bdbcb8ce7
prikol769 <prikol769@gmail.com>
2023-10-03 10:05:51 -0500

    added Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import Nav from "./components/Nav";
 import { Hero, CustomerReviews, Footer, PopularProducts, Services, SpecialOffer, Subscribe, SuperQuality } from "./sections";
 
 const App = () => (
   <main className="relative">
-    Nav
+    <Nav />
     <section className="xl:padding-1 wide:padding-r padding-b">
       <Hero />
     </section>
